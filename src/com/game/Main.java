e84fc65b2af9056cf2cd0cf2cafa87e4167294b2
DanovanGian <dodogian005@gmail.com>
2026-06-09 23:15:14 +0700

    tambah player

--- src/com/game/Main.java
+++ src/com/game/Main.java
@@ -31,9 +31,9 @@ class Player {
         System.out.println("Level    : " + this.level);
         System.out.println("Health    : " + this.helath + " hp");
         System.out.println("\n");
-        this.arrmor.ShowArrmor();
-        System.out.println("\n");
         this.senjata.ShowWeapon();
+        System.out.println("\n");
+        this.arrmor.ShowArrmor();
         System.out.println("====================");
 
     }
@@ -81,19 +81,45 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //object pemain 1
-        Player Pemain1 = new Player("Gian Danovan", 1, 98);
-        //obect Arrmor 1
-        Arrmor Arrmor1 = new Arrmor("Diamond Helmet", 2, 200);
-        //object senjata 1
-        Weapon Senjata1 = new Weapon ("Diamond Sword", 2, 25);
+        //object pemain 
+        Player Player1 = new Player("Gian Danovan", 1, 98);
+        Player Player2 = new Player("Steve", 5,100);
+        Player Player3 = new Player("Nicholas", 10,50);
 
-        //
-        Pemain1.pakaiSenjata(Senjata1);
-        Pemain1.pakaiArrmor(Arrmor1);
+
+
+        //object senjata 
+        Weapon DiamondSword = new Weapon ("Diamond Sword", 2, 250);
+        Weapon Panah = new Weapon ("Panah", 2, 50);
+        Weapon Kerikil = new Weapon ("Kerikil", 1, 10);
+        Weapon Ketapel = new Weapon ("Ketapel", 3, 25);
+        //obect Arrmor 
+        Arrmor DiamondHelmet = new Arrmor("Diamond Helmet", 2, 200);
+        Arrmor BajuKulit = new Arrmor("Baju Kulit", 2, 50);
+        Arrmor BajuBesi = new Arrmor("Baju Besi", 2, 100);
+        Arrmor BaJuEmas = new Arrmor("Baju Emas", 2, 150);
+       
+
+        //Player1 Atribut pakai
+        Player1.pakaiSenjata(DiamondSword);
+        Player1.pakaiArrmor(DiamondHelmet);
+
+        //Player2 Atribut pakai
+        Player1.pakaiSenjata(Ketapel);
+        Player1.pakaiArrmor(DiamondHelmet);
+
+        //Player2 Atribut pakai
+        Player1.pakaiSenjata(Kerikil);
+        Player1.pakaiArrmor(BajuKulit);
 
         //memenggil method yang menampilkan data 
-        Pemain1.ShowPlayer();
+        Player1.ShowPlayer();
+        System.out.println("\n");
+
+        Player2.ShowPlayer();
+        System.out.println("\n");
+
+        Player3.ShowPlayer();
         System.out.println("\n");
 
     }

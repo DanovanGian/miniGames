b529d01b9965a82292b22ac1d67053a03a981956
DanovanGian <dodogian005@gmail.com>
2026-06-17 22:10:13 +0700

    ayo kita mulai

--- src/com/game/Main.java
+++ src/com/game/Main.java
@@ -42,10 +42,15 @@ public class Main {
         // fitur bertempur
         System.out.println("\nPERTEMPURAN DI MULAI!! \n");
 
+        //fase 1
         Player1.Serangan(Player2);
-        System.out.println("SELANJUTNYA AKAN MENAMBAH FITUR SALING SERANG");
-        System.out.println("AKAN ADA PENYESUAIAN NILAI SERANGAN DAN DFENSE");
-        System.out.println("OKE DONE SELANJUTNYA AKAN SAYA TAMBAHKAN FITUR UNTUK MELAKUKAN PERANG TERUS MENERUS SAMPAI HP SALAH SATU PLAYER HABIS");
+        System.out.println("\n");
+        //fase 2
+        Player2.Serangan(Player1);
+
+        // System.out.println("SELANJUTNYA AKAN MENAMBAH FITUR SALING SERANG");
+        // System.out.println("AKAN ADA PENYESUAIAN NILAI SERANGAN DAN DFENSE");
+        // System.out.println("OKE DONE SELANJUTNYA AKAN SAYA TAMBAHKAN FITUR UNTUK MELAKUKAN PERANG TERUS MENERUS SAMPAI HP SALAH SATU PLAYER HABIS");
 
     }
 }
